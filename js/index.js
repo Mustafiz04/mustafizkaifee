938d606c0082e75f7939202e327c8b2d4290089e
Mustafiz04 <kaifee000@gmail.com>
2020-03-28 20:43:04 +0530

    Skills Set

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var icon = document.getElementById('moon')
-var lay = document.getElementById('lay');
+var lay = document.getElementById('page-content');
 var list = document.querySelectorAll('#list')
 var heading = document.getElementById("heading");
 var social = document.querySelectorAll(".social-icon");
@@ -8,23 +8,22 @@ var dash = document.getElementById('dash');
 function changeImage(){
     imgsrc = document.getElementById('moon').src;
     if( imgsrc.indexOf("icon/moon.png") != -1 ){
-        console.log(list);
         icon.src = "icon/sun.png";
         document.body.style.backgroundColor = 'white';
         list.forEach(i => {
             i.style.color = '#000000';
         })
-        lay.style.color = 'black';
         heading.style.color = 'rgb(41, 38, 62)';
         dash.style.color = 'black';
+        lay.style.color = 'black';
     }else{
         icon.src = "icon/moon.png";
         document.body.style.backgroundColor = 'rgb(41, 38, 62)';
-        lay.style.color = 'white';
         list.forEach(i => {
             i.style.color = 'white';
         })
         heading.style.color = 'rgb(130, 187, 207)';
         dash.style.color = 'white';
+        lay.style.color = 'white';
     }   
 }
